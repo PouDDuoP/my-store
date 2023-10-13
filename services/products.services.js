75700f7d48b72a07d688aff2235e281ead105fd0
PouDDuoP <kevinalvarado.ag@gmail.com>
2023-10-12 22:17:56 -0400

    Async await y captura de errores
    Se aplicaron cambios de Async await, Promes y try catch, para trabajar de manera asincrona las peticiones a products

--- services/products.services.js
+++ services/products.services.js
@@ -21,7 +21,7 @@ class ProductsService {
     }
   }
 
-  create(data) {
+  async create(data) {
     const newProduct = {
       id: faker.datatype.uuid(),
       ...data
@@ -30,15 +30,19 @@ class ProductsService {
     return newProduct;
   }
 
-  find() {
-    return this.products;
+  async find() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(this.products);
+      }, 5000);
+    })
   }
 
-  findOne(id) {
+  async findOne(id) {
     return this.products.find(item => item.id === id)
   }
 
-  update(id, change) {
+  async update(id, change) {
     const index = this.products.findIndex(item => item.id === id);
     if (index === -1)
     {
@@ -52,7 +56,7 @@ class ProductsService {
     return this.products[index];
   }
 
-  delete(id) {
+  async delete(id) {
     const index = this.products.findIndex(item => item.id === id);
     if (index === -1)
     {
