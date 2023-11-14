e16fc67eadb7358053ea198e35cc6d21a6aba9c2
PouDDuoP <kevinalvarado.ag@gmail.com>
2023-11-13 22:24:43 -0400

    Cambios a My Sql
    Se realizaron ajustes para trabajar con my mysql, tambien se quita del gitignore el archivo .env para tener un respaldo en git, cabe destacar que el archivo .env por defecto esta en agregado al gitignore

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const routerApi = require("./routes");
 
-const { logErrors, errorHandler, boomErrorHandler } = require('./middleware/error.handler')
+const { logErrors, errorHandler, boomErrorHandler, ormErrorHandler } = require('./middleware/error.handler')
 
 const app = express();
 const port = 3000;
@@ -33,6 +33,7 @@ app.get('/new-route', (req, res) => {
 routerApi(app);
 
 app.use(logErrors);
+app.use(ormErrorHandler);
 app.use(boomErrorHandler);
 app.use(errorHandler);
 
--- middleware/error.handler.js
+++ middleware/error.handler.js
@@ -1,10 +1,13 @@
-function logErrors(err, req, res, next)  {
+const { ValidationError } = require("sequelize");
+const boom = require('@hapi/boom');
+
+function logErrors(err, req, res, next) {
   console.log('logErrors');
   console.error(err);
   next(err);
 }
 
-function errorHandler(err, req, res, next)  {
+function errorHandler(err, req, res, next) {
   console.log('errorHandler');
   res.status(500).json({
     message: err.message,
@@ -12,7 +15,7 @@ function errorHandler(err, req, res, next)  {
   })
 }
 
-function boomErrorHandler(err, req, res, next)  {
+function boomErrorHandler(err, req, res, next) {
   console.log('boomErrorHandler');
   console.log(err.isBoom);
   console.log('boomErrorHandler');
@@ -25,4 +28,15 @@ function boomErrorHandler(err, req, res, next)  {
   }
 }
 
-module.exports = { logErrors, errorHandler, boomErrorHandler }
+function ormErrorHandler(err, req, res, next) {
+  if (err instanceof ValidationError) {
+    res.status(409).json({
+      statusCode: 409,
+      message: err.name,
+      errors: err.errors
+    });
+  }
+  next(err);
+}
+
+module.exports = { logErrors, errorHandler, boomErrorHandler, ormErrorHandler }
