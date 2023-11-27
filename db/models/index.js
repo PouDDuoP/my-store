64b092dabead7b8d0a62db99c95808ca1af52403
PouDDuoP <kevinalvarado.ag@gmail.com>
2023-11-27 08:34:38 -0400

    Update index.js
    Faltaba hacer la configuración del modelo tier en el index.js de models

--- db/models/index.js
+++ db/models/index.js
@@ -1,15 +1,18 @@
 const { User, UserSchema } = require('./user.model');
 const { Category, CategorySchema } = require('./category.model');
 const { Product, ProductSchema } = require('./product.model');
+const { Tier, TierSchema } = require('./tier.model');
 
 function setupModels(sequelize) {
   User.init(UserSchema, User.config(sequelize));
   Category.init(CategorySchema, Category.config(sequelize));
   Product.init(ProductSchema, Product.config(sequelize));
+  Tier.init(TierSchema, Tier.config(sequelize));
 
   // relationship
   Product.associate(sequelize.models);
   Category.associate(sequelize.models);
+  Tier.associate(sequelize.models);
 }
 
 module.exports = setupModels;
