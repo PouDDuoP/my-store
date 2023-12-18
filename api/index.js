81bdc5922b1acd2c79cde3dfa73ac59eeefc8aff
PouDDuoP <kevinalvarado.ag@gmail.com>
2023-12-18 09:36:53 -0400

    Swagger configuración en proceso
    Se estan trabajando ajustes en el swagger

--- api/index.js
+++ api/index.js
@@ -1,15 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const routerApi = require("./routes");
+const { swaggerDocs: V1SwaggerDocs } = require('./routes/swagger');
 
 const { logErrors, errorHandler, boomErrorHandler, ormErrorHandler } = require('./middleware/error.handler');
 
 const app = express();
-
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument  = require('swagger-jsdoc');
-
-// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const port = process.env.PORT || 3000;
 
@@ -27,20 +23,6 @@ const options = {
 }
 app.use(cors(options));
 
-// app.use('/api-docs', swaggerUi.serve);
-// app.get('/api-docs', swaggerUi.setup(swaggerDocument));
-
-var options_ = {
-  explorer: true,
-  swaggerOptions: {
-    url: 'http://localhost:3000/api/v1/'
-  }
-}
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(null, options_));
-
-// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
 app.get('/api/', (req, res) => {
   res.send('Hola este el servicio express');
 });
@@ -57,7 +39,8 @@ app.use(boomErrorHandler);
 app.use(errorHandler);
 
 app.listen(port, () => {
-  console.log('My port '+ port);
+  console.log(`My port ${port}`);
+  V1SwaggerDocs(app, port);
 });
 
 // console.log('My App');
--- api/routes/swagger.js
+++ api/routes/swagger.js
@@ -1,3 +1,72 @@
-const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// app.use('/api-docs', swaggerUi.serve);
+// app.get('/api-docs', swaggerUi.setup(swaggerDocument));
+
+const swaggerDefinition = {
+  explorer: true,
+  openapi: "3.1.0",
+  info: {
+    title: "My Store Express API with Swagger",
+    version: "1.0.0",
+    description:
+      "This is a simple CRUD API application made with Express and documented with Swagger",
+    // license: {
+    //   name: "MIT",
+    //   url: "http://localhost:3000",
+    // },
+    // contact: {
+    //   name: "MyStore",
+    //   url: "http://localhost:3000",
+    //   email: "kkk@email.com",
+    // },
+  },
+  servers: [
+    {
+      url: "http://localhost:3000/api/v1/",
+    },
+  ],
+  schemes: ['http'],
+  host: 'localhost:3000',
+  basePath: '/api/v1',
+  // paths: {
+  //   path: {
+  //     '/categories/': [Object]
+  //   }
+  // },
+};
+
+const swaggerOptions = {
+  swaggerDefinition,
+  // apis: ["./api/routes/index.js"]
+  apis: ["./api/routes/*.js"]
+};
+
+const swaggerSpecs = swaggerJsDoc(swaggerOptions);
+
+const swaggerDocs = (app, port) => {
+  app.use("/api/v1/docs",
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerSpecs, { explorer: true })
+  );
+  app.get("/api/v1/docs.json", (req, res) => {
+    res.setHeader("Content-Type", "application/json");
+    res.send(swaggerSpecs);
+  });
+
+  console.log(
+    `Version 1 Docs are available at http://localhost:${port}/api/v1/docs`
+  );
+  
+}
+
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(null, options_));
+
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// https://www.youtube.com/watch?v=RayDPBYou4I
+
+module.export = { swaggerDocs }
