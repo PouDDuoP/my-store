4bb4d3e6a1bd82a232737bcb9acb02afb941a6bb
PouDDuoP <kevinalvarado.ag@gmail.com>
2023-12-22 10:32:54 -0400

    Update entrypoint.sh
    Agregadas lineas para desinstalar e instalar bcrypt por un error al tratar de ejecutar. Queda pendiente por verificar.

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,6 +3,13 @@ set -e           # Stop on any error
 # npx sequelize-cli db:migrate:undo --name 20231121160107-create-user utilizado para reversar una migracion creada por error
 # npm run migrations:revert --name 20231121203812-add-profile.js
 # npm run migrations:delete  # Run undo all migrations
+
+# TODO: queda pendiente por verificar ya que se agregaron estas lienas de bcrypt para solucionar un error pero no deberia quedar asi.
+# uninstall the current bcrypt modules
+npm uninstall bcrypt
+# install the bcrypt modules for the machine
+npm install bcrypt
+
 npm run migrations:run  # Run migrations
 # npm run seed     # Preload initial data
 exec "$@"
