78f4c6bb7d4c9698ad10fc55d0c59c86debac303
kevin alvarado <kevinalvarado.ag@gmail.com>
2023-12-27 15:19:10 -0400

    Update products.router.js
    Agregado autenticación con JWT para los métodos POST, PATCH, DELETE.

--- api/routes/products.router.js
+++ api/routes/products.router.js
@@ -148,6 +148,8 @@
 */
 
 const express = require("express");
+const passport = require("passport");
+
 const ProductsService = require("../services/product.services");
 const validatorHandler = require("../middleware/validator.handler");
 const { createProductSchema, updateProductSchema, getProductSchema, queryProductSchema } = require("../schemas/product.schema");
@@ -185,6 +187,7 @@ router.get('/:id',
 );
 
 router.post('/',
+  passport.authenticate('jwt', {session: false}),
   validatorHandler(createProductSchema, 'body'),
   async (req, res, next) => {
     try {
@@ -199,6 +202,7 @@ router.post('/',
 );
 
 router.patch('/:id',
+  passport.authenticate('jwt', {session: false}),
   validatorHandler(getProductSchema, 'params'),
   validatorHandler(updateProductSchema, 'body'),
   async (req, res, next) => {
@@ -224,6 +228,7 @@ router.patch('/:id',
 // });
 
 router.delete('/:id',
+  passport.authenticate('jwt', {session: false}),
   validatorHandler(getProductSchema, 'params'),
   async (req, res, next) => {
     try {
