0dc2cc9b992b6aba86e7a4a9cd706e06eadbcef0
PouDDuoP <kevinalvarado.ag@gmail.com>
2024-01-09 00:23:05 -0400

    Fix strategy local
    Se agrega parámetro faltante.

--- api/utils/auth/strategies/local.strategy.js
+++ api/utils/auth/strategies/local.strategy.js
@@ -8,7 +8,7 @@ const LocalStrategy = new Strategy({
   passwordField: 'password'
 }, async (username, password, done) => {
   try {
-    const user = await service.getUser(username);
+    const user = await service.getUser(username, password);
     done(null, user);
   } catch (error) {
     done(error, false);
